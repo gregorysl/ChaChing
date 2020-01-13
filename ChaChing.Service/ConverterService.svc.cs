b7bff95ea00fc5877254edd2c583462675d1bc6b
gregorysl <gregorysl@gmail.com>
2020-01-13 15:46:24 +0100

    Added base WCF service

---
+++ ChaChing.Service/ConverterService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChaChing.Service
+{
+    public class ConverterService : IConverterService
+    {
+        public string GetData(int value)
+        {
+            return $"You entered: {value}";
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException(nameof(composite));
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ ChaChing.Service/IConverterService.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace ChaChing.Service
+{
+    [ServiceContract]
+    public interface IConverterService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        [DataMember]
+        public bool BoolValue { get; set; } = true;
+
+        [DataMember]
+        public string StringValue { get; set; } = "Hello ";
+    }
+}
